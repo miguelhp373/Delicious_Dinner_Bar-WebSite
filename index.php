0ebac893af7646febff2b7dc95f68d131462ee33
Miguel Henrique <miguelhp373@gmail.com>
2021-01-22 16:20:15 -0300

    Foi Feito o WireFrame do Projeto

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+    include('components/header.php');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="components/styles/header.css">
+</head>
+<body>
+    
+</body>
+</html>
