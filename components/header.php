562e89b973c72b729ce85cb09f640b09402ff51a
Miguel Henrique <miguelhp373@gmail.com>
2021-01-25 21:05:03 -0300

    home section Updates - 0.1.2

--- components/header.php
+++ components/header.php
@@ -1,15 +1,50 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">    
-</head>
-<body>
-        <div class="nav-bar-top-fixed">
-            <span>nome</span>
-        </nav>
+   <div class="nav-bar-transparent">
+       <nav class="hamburger-responsive-menu">
+           <input type="checkbox" id="check">
+           <label class="button-icon" for="check"><i class="fa fa-bars" aria-hidden="true"></i></label>
 
- 
-</body>
-
-</html>
+           <div class="left-bar">
+               <nav class="links">
+                   <a href="#">
+                       <div class="text1">texto1</div>
+                   </a>
+                   <a href="#">
+                       <div class="text2">texto2</div>
+                   </a>
+                   <a href="#">
+                       <div class="text3">texto3</div>
+                   </a>
+               </nav>
+           </div>
+       </nav>
+       <div class="logo-brand-top">
+           <h1>Delicious Dinner Bar.</h1>
+       </div>
+       <div class="menu-top-horizontal">
+           <ul>
+               <a href="#">
+                   <li class="links-menu">Home</li>
+               </a>
+               <a href="#">
+                   <li class="links-menu">About</li>
+               </a>
+               <a href="#">
+                   <li class="links-menu">Reservation</li>
+               </a>
+               <a href="#">
+                   <li class="links-menu">Menu</li>
+               </a>
+               <a href="#">
+                   <li class="links-menu">Shop</li>
+               </a>
+           </ul>
+       </div>
+   </div>
+   <div id="background-home">
+       <img src="https://i.imgur.com/g3MDTkJ.jpg" alt="home-image">
+   </div>
+   <div class="title-home">
+       <h1>Making Delicious</h1>
+       <h2>Gourmet Fast Food</h2>
+       <button>Explore Now</button>
+   </div>
